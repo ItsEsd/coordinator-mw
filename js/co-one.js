32c3adadb30da74721068e197307e42638ff59b3
ItsEsd <81977626+ItsEsd@users.noreply.github.com>
2022-03-05 17:54:00 +0530

    Update co-one.js

--- js/co-one.js
+++ js/co-one.js
@@ -77,7 +77,7 @@ function sendEmail() {
   var mailat =  document.getElementById('mailrepass').value;
   document.getElementById('vercodepre').value = k;
     Email.send({
-      SecureToken : "6c70cce5-9887-4231-b703-afed94e0bd85",
+      SecureToken : "cc4823c2-6012-4e3c-b375-9fa519b23754",
         To: mailat,
         From: "noreply@mastrowall.com",
         Subject: "Email Verification",
